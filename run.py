eef57a3b32995fa72e3cd46fa99f2668c7453add
bezabu <>
2023-10-22 10:29:08 +0000

    Add requirements for deplotment

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -24,7 +23,7 @@ def get_sales_data():
         print("Data should be six numbers, seperated by commas.")
         print("Example@ 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n")
     
         sales_data = data_str.split(",")
 
@@ -122,9 +121,9 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
-    
 
 
 print("Welcome to Love Sandwiches Data Automation")
+
 main()
 
